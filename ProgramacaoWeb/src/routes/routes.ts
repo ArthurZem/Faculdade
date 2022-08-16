c057198a96263419b703154f5975ea326f544903
Arthur Zem de Souza <arthurzem1612@gmail.com>
2022-08-16 12:16:37 -0300

    Atualização

---
+++ ProgramacaoWeb/src/routes/routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import hotelController from './controller/hotel';
+import quartoController from './controller/quarto';
+import telefoneController from './controller/telefone';
+import enderecoController from './controller/endereco';
+
+const routes = Router();
+
+routes.get('/', (req, res) => {
+return res.send('Hello World');
+})
+
+routes.get('/hotels', hotelController.index); // para obter todos os hoteis
+routes.get('/hotel/:id', hotelController.get); // para obter um hotel pelo campo id
+routes.put('/hotel/:id', hotelController.add); // para altera os atributos o hotel pelo campo id
+routes.delete('/hotel/:id', hotelController.delete); // para deletar (apagar) um hotel pelo campo id
+routes.post('/hotel/:id', hotelController.update); // para criar (incluir) um hotel pelo campo id
+
+routes.get('/quartos', quartoController.index);
+routes.get('/quarto/:id', quartoController.get);
+routes.put('/quarto/:id', quartoController.add);
+routes.delete('/quarto/:id', quartoController.delete);
+routes.post('/quarto/:id', quartoController.update);
+
+routes.get('/telefones', telefoneController.index);
+routes.get('/telefone/:id', telefoneController.get);
+routes.put('/telefone/:id', telefoneController.add);
+routes.delete('/telefone/:id', telefoneController.delete);
+routes.post('/telefone/:id', telefoneController.update);
+
+routes.get('/enderecos', enderecoController.index);
+routes.get('/endereco/:id', enderecoController.get);
+routes.put('/endereco/:id', enderecoController.add);
+routes.delete('/endereco/:id', enderecoController.delete);
+routes.post('/endereco/:id', enderecoController.update);
+
+export default routes;
---
+++ app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { ServerApiVersion } from 'mongodb';
+import routes from '../src/routes';
+
+class App {
+public express: express.Application;
+
+public constructor() {
+this.express = express();
+this.express.use(cors());
+
+this.middlewares();
+this.initMongoose();
+this.connectDatabase();
+this.routes();
+}
+
+private initMongoose() {
+mongoose.set('runValidators', true);
+}
+
+private middlewares(): void {
+this.express = express();
+this.express.use(express.json());
+this.express.use(cors());
+}
+
+private routes() {
+this.express.use(routes);
+}
+
+private connectDatabase(): void {
+const uri = "mongodb+srv://USER:SENHA@cluster0.c0lb9.mongodb.net/XXXX?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+serverApi: ServerApiVersion.v1
+}).then(() => {
+console.log('Conectado ao mongodb');
+// cria_tabela()
+}).catch((error) => {
+console.log(error);
+});
+}
+
+public listen(port: number): void {
+this.express.listen(port, () => {
+console.log(`Aplicação iniciada na porta : ${port}`);
+});
+}
+}
+
+export default new App();
